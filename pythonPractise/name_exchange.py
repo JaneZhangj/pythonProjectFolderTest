750e91cfdbc7a243f0b773c6ce2e5ebc9dc0fd08
JaneZhangj <2394764828@qq.com>
2018-06-07 18:20:16 +0800

    驼峰名与下划线命名转换

--- pythonPractise/name_exchange.py
+++ pythonPractise/name_exchange.py
@@ -1,6 +1,5 @@
 import re
 
-
 def hump2underline(hunp_str):
     '''
     驼峰形式字符串转成下划线形式
@@ -12,7 +11,6 @@ def hump2underline(hunp_str):
     # 使用正则分组的后向引用
     sub = re.sub(p, r'\1_\2', hunp_str).lower()
     return sub
-
 
 def underline2hump(underline_str):
     '''
@@ -26,7 +24,7 @@ def underline2hump(underline_str):
 
 
 def test_hump2underline():
-    # 供测试用的一些驼峰形式的字符串
+    # 供测试用的一些驼峰命名形式的字符串
     attr1 = 'PersonNamePattern'
     attr2 = 'IPv6Address'
     attr3 = 'personDetailInfo'
